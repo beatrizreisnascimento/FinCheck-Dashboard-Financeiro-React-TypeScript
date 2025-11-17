48f3675717e9c291c375acdddfbbae807ce614ae
Beatriz Reis do Nascimento <beatrizreis144@gmail.com>
2025-11-17 14:19:07 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: 'Arial' }}>
+      <h1>FinCheck</h1>
+      <p>Dashboard financeiro simples desenvolvido em React.</p>
+    </div>
+  );
+}
